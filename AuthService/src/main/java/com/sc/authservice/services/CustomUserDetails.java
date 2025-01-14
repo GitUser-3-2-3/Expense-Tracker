8fd3a68fa1e3bf5f57d0dc4db8f176436c31a0b2
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-01-14 22:55:01 +0530

    initializes user for internal processing

---
+++ AuthService/src/main/java/com/sc/authservice/services/CustomUserDetails.java
@@ -0,0 +1,45 @@
+package com.sc.authservice.services;
+
+import com.sc.authservice.entities.UserInfo;
+import com.sc.authservice.entities.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails extends UserInfo implements UserDetails {
+
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    private final String username;
+    private final String password;
+
+    public CustomUserDetails(UserInfo byUsername) {
+        this.username = byUsername.getUsername();
+        this.password = byUsername.getPassword();
+
+        List<GrantedAuthority> authList = new ArrayList<>();
+        for (UserRole role : byUsername.getRoles()) {
+            authList.add(new SimpleGrantedAuthority(role.getRoleName().toUpperCase()));
+        }
+        this.authorities = authList;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+}
