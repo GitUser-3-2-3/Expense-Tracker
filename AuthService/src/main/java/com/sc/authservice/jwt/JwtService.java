fc36fc9a1c985635f2422d49d7e105440b462484
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-01-14 21:48:17 +0530

    standard implementation of JwtService, modified yaml file for secret key config.

---
+++ AuthService/src/main/java/com/sc/authservice/jwt/JwtService.java
@@ -0,0 +1,63 @@
+package com.sc.authservice.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private static String SECRET_KEY;
+
+    public String createToken(Map<String, Object> extraClaims, String username) {
+
+        return Jwts.builder().claims(extraClaims).subject(username)
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + 864000))
+            .signWith(getSignInKey())
+            .compact();
+    }
+
+    public String extractUsername(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getExpiration);
+    }
+
+    public Boolean isTokenExpired(String jwtToken) {
+        return extractExpiration(jwtToken).before(new Date());
+    }
+
+    public Boolean validateToken(String jwtToken, UserDetails userDetails) {
+        final String username = extractUsername(jwtToken);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(jwtToken);
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts.parser().verifyWith(getSignInKey()).build()
+            .parseSignedClaims(jwtToken)
+            .getPayload();
+    }
+
+    public <T> T extractClaim(String jwtToken, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(jwtToken);
+        return claimResolver.apply(claims);
+    }
+}
