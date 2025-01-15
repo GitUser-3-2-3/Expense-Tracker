38ead2859dd431032f7dffe8f7278c33e0d7ad88
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-01-15 15:53:58 +0530

    separated the logic in a service class

--- AuthService/src/main/java/com/sc/authservice/controller/AuthController.java
+++ AuthService/src/main/java/com/sc/authservice/controller/AuthController.java
@@ -1,11 +1,7 @@
 package com.sc.authservice.controller;
 
-import com.sc.authservice.entities.RefreshToken;
-import com.sc.authservice.jwt.JwtService;
-import com.sc.authservice.jwt.RefreshTokenService;
 import com.sc.authservice.model.UserInfoDTO;
-import com.sc.authservice.response.JwtResponseDTO;
-import com.sc.authservice.services.UserDetailsServiceImpl;
+import com.sc.authservice.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,42 +10,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("auth/v1/")
 public class AuthController {
 
-    private final UserDetailsServiceImpl userDetailsService;
-    private final RefreshTokenService refreshTokenService;
-    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
 
     @Autowired
-    public AuthController(
-        UserDetailsServiceImpl userDetailsService, RefreshTokenService refreshTokenService, JwtService jwtService) {
-        this.userDetailsService = userDetailsService;
-        this.jwtService = jwtService;
-        this.refreshTokenService = refreshTokenService;
+    public AuthController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
     }
 
     @PostMapping("signup")
     public ResponseEntity<?> signup(@RequestBody UserInfoDTO userInfoDTO) {
         try {
-            Boolean isSignedUp = userDetailsService.signupUser(userInfoDTO);
-            if (Boolean.FALSE.equals(isSignedUp)) {
-                return new ResponseEntity<>("User already exists.", HttpStatus.BAD_REQUEST);
-            }
-            RefreshToken refreshToken = refreshTokenService.createRefreshToken(userInfoDTO
-                .getUsername());
-            String jwtToken = jwtService.createToken(new HashMap<>(), userInfoDTO.getUsername());
-
-            JwtResponseDTO jwtResponseDTO = JwtResponseDTO.builder().accessToken(jwtToken)
-                .refreshToken(refreshToken.getToken()).build();
-
-            return new ResponseEntity<>(jwtResponseDTO, HttpStatus.OK);
+            Object user = authenticationService.signup(userInfoDTO);
+            return new ResponseEntity<>(user, CREATED);
         }
         catch (Exception rtExp) {
-            return new ResponseEntity<>("Exception in user service", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Exception in user service", INTERNAL_SERVER_ERROR);
         }
     }
 }
